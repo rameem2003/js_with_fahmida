3262fe06971262d74072e253fb6dd9a1b819b837
rameem2003 <rameem2019@gmail.com>
2022-10-17 21:36:31 +0600

    update

--- forloop.js
+++ forloop.js
@@ -9,6 +9,42 @@
 
 
 
-for(let fahmida = 500; fahmida >= 0; fahmida--){
-    console.log(fahmida);
+// for(let fahmida = 500; fahmida >= 0; fahmida--){
+//     console.log(fahmida);
+// }
+
+
+// for(let a = 0; a <= 100; a++){
+//     // loop body
+//     console.log(a);
+// }
+
+// while loop
+
+// let x = 0;
+
+// while(x <= 100){
+//     console.log(x);
+//     x++;
+// }
+
+
+
+
+let sum = 0;
+let x = 100;
+
+while(x >= 0){
+    console.log(x);
+    x = x - 2;
 }
+
+
+// for(let x = 1; x <= 100; x++){
+//     sum = sum + x;
+//     console.log(sum);
+// }
+
+
+
+
