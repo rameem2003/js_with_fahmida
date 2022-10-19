bdccbe097d30d5603944257b2aada9fd495246d4
rameem2003 <rameem2019@gmail.com>
2022-10-19 21:27:33 +0600

    update game relase

--- guessing_game/app.js
+++ guessing_game/app.js
@@ -6,10 +6,39 @@ const resetBtn = document.getElementById("resetBtn");
 
 
 startBtn.addEventListener("click", () => {
+    let numberOfwon = 0;
+    let numberOflose = 0;
+
+    for(let x = 1; x <= 5; x++){
+        let inputNumber = parseInt(prompt("Enter number from 1 to 5"))
+
+        let systemNumber = Math.floor(Math.random() * 5) + 1;
+
+        if(inputNumber == systemNumber){
+            console.log("আই জিতছি");
+            numberOfwon++
+        }else{
+            console.log("আই হারি গেছি");
+            numberOflose++
+        }
+    }
+
     icon.style.display = "block"
     formateText.style.display = "block"
     startBtn.style.display = "none"
     resetBtn.style.display = "initial"
+
+
+    if(numberOfwon > numberOflose){
+        formateText.innerHTML = `আই জিতছি ${numberOfwon} বার`
+        icon.classList.replace("fa-heart-crack", "fa-medal")
+
+    }else{
+        formateText.innerHTML = `আই হারি গেছি ${numberOflose} বার`
+        icon.classList.replace("fa-medal", "fa-heart-crack")
+    }
+
+    
 })
 
 resetBtn.addEventListener("click", () => {
