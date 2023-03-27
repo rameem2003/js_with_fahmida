9de3b06289322a5c87d029af12beccf4ef1645c7
rameem2003 <rameem2019@gmail.com>
2023-03-27 22:10:49 +0600

    update for fahmida

--- fetch_api_js/app.js
+++ fetch_api_js/app.js
@@ -12,7 +12,33 @@ searchBtn.addEventListener("click", () => {
 
     let countryName = inputCountry.value;
 
-    fetchUniversity(countryName);
+    if(countryName != ""){
+        fetchUniversity(countryName);
+    }else{
+        cards.innerHTML = `<div class="preMsg">
+                            <h1>Please Enter The value !!!! 🤷‍♂️</h1>
+                    </div>`
+    }
+})
+
+
+inputCountry.addEventListener("keyup", e => {
+    console.log(e);
+
+    if(e.key === "Enter"){
+        search_box.classList.add("hide");
+        cards.classList.add("show");
+
+        let countryName = inputCountry.value;
+
+        if(countryName != ""){
+            fetchUniversity(countryName);
+        }else{
+            cards.innerHTML = `<div class="preMsg">
+                                <h1>Please Enter The value !!!! 🤷‍♂️</h1>
+                        </div>`
+        }
+    }
 })
 
 
@@ -21,18 +47,35 @@ searchBtn.addEventListener("click", () => {
 
 
 function fetchUniversity (country) {
+    cards.innerHTML = `<div class="preMsg">
+                            <h1>Please Wait.......</h1>
+                    </div>`
+
     fetch(`http://universities.hipolabs.com/search?country=${country}`).then(response => response.json()).then(data => {
         console.log(data);
 
-        cards.innerHTML = `${data.map(element => {
-            return `<div class="card">
-                        <h1>${element.name}</h1>
-
-                        <div class="flex">
-                            <span>${element.country}</span>
-                            <a href="${element.web_pages[0]}">${element.web_pages[0]}</a>
-                        </div>
+        if(data.length != 0){
+            cards.innerHTML = data.map(element => {
+                return `<div class="card">
+                            <h1>${element.name}</h1>
+    
+                            <div class="flex">
+                                <span>${element.country}</span>
+                                <a href="${element.domains[0]}">University Website Link</a>
+                            </div>
+                        </div>`
+            }).join(" ");
+        }else{
+            cards.innerHTML = `<div class="preMsg">
+                            <h1>Invalid Query</h1>
                     </div>`
-        })}`
+        }
+
+        
     })
 }
+
+
+// const array = ["rameem", "fahmida"];
+
+// cards.innerHTML = array.join(" ");
