80d838594c7e30f1d435311773052b0a70f90772
rameem2003 <rameem2019@gmail.com>
2023-04-01 21:30:55 +0600

    update

--- weather_app/app.js
+++
@@ -1,8 +0,0 @@
-fetch(`https://api.openweathermap.org/data/2.5/weather?q=dhaka&units=metric&appid=7a99801248ad653462c72a0c01c29959`).then(responce => responce.json()).then(data => {
-    console.log(data);
-
-    let sunriseUTC = data.sys.sunrise;
-    let timeZone = data.timezone;
-    let date = new Date(sunriseUTC * 1000)
-    console.log(date);
-})
